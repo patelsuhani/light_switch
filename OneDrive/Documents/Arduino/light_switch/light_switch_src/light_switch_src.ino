dfa53bbe9c7f5f18bce1d706a8c8b7be35ee6e18
patelsuhani <121316408+patelsuhani@users.noreply.github.com>
2024-03-13 01:26:34 -0400

    configured digital pins

---
+++ OneDrive/Documents/Arduino/light_switch/light_switch_src/light_switch_src.ino
@@ -0,0 +1,13 @@
+int switchState = 0;
+
+void setup() {
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode (2,INPUT);  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
